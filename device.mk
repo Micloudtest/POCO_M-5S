04354664494255cef8daaae135031b70618d8e7f
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2025-01-01 20:39:22 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -88,10 +88,10 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libkeymaster41 \
     libpuresoftkeymasterdevice \
-    android.hardware.vibrator-V1-ndk_platform
+    android.hardware.vibrator-V2-ndk_platform
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V1-ndk_platform.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.vibrator-V2-ndk_platform.so
