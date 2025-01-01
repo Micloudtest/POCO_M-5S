f776e6c88895131d704a8c9fd76ec6cd56148d91
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2025-01-01 16:36:57 +0800

    Update twrp_rosemary.mk

--- twrp_rosemary.mk
+++ twrp_rosemary.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from rosemary device
-$(call inherit-product, device/redmi/rosemary/device.mk)
+$(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 # Inherit some common TWRP stuff
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -32,8 +32,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rosemary
 PRODUCT_NAME := twrp_rosemary
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := rosemary
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 10S/POCO M5s
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_RELEASE_NAME := Redmi Note 10S
 
