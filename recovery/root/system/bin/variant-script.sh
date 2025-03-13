5a59b356e72b583e65342184b060121c55a5f2e0
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2025-03-13 17:32:12 +0800

    Update variant-script.sh

--- recovery/root/system/bin/variant-script.sh
+++ recovery/root/system/bin/variant-script.sh
@@ -54,18 +54,18 @@ do_rosemary() {
 }
 
 do_rosemary_p() {
-    setdevicename "rosemary_p"
+    setdevicename "rosemary"
     setdevicemodel "POCO M5s"
 }
 
 do_maltose() {
     setdevicename "maltose"
-    setdevicemodel "Redmi Note 10s"
+    setdevicemodel "Redmi Note 10S"
 }
 
 do_secret() {
     setdevicename "secret"
-    setdevicemodel "Redmi Note 10s"
+    setdevicemodel "Redmi Note 10S"
 }
 
 do_secret_in2() {
