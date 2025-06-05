b5a1a3161ce8313daa88b0d2069736b164fff20f
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2025-06-06 07:39:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ DEVICE_PATH := device/xiaomi/rosemary
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Dynamic
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
