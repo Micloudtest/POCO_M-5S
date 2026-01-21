f8ecd595c9573cb7a4af475dda91d657ac096654
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2026-01-22 02:31:43 +0800

    Disable flashlight feature in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -71,7 +71,7 @@ if [ -f "$(gettop)/bootable/recovery/orangefox.cpp" ]; then
 	  	export OF_HIDE_NOTCH=1
 	  	export OF_CLOCK_POS=1 # left and right clock positions available
 	  	export OF_USE_GREEN_LED=0
-	  	export OF_FLASHLIGHT_ENABLE=1
+	  	export OF_FLASHLIGHT_ENABLE=0
 	  	export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6768.rc for more information
 
 	  	# Binaries
