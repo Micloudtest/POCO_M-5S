628040bab306c3b672a3e9a82e615e1555697c34
Micloudtest <145197598+Micloudtest@users.noreply.github.com>
2026-02-06 00:15:08 +0800

    Update variant-script.sh

--- recovery/root/system/bin/variant-script.sh
+++ recovery/root/system/bin/variant-script.sh
@@ -22,16 +22,17 @@
 #
 
 setdevicename() {
+    resetprop "ro.build.product" "$1"
     resetprop "ro.product.device" "$1"
     resetprop "ro.product.vendor.device" "$1"
+    resetprop "ro.product.system.device" "$1"
     resetprop "ro.product.odm.device" "$1"
-    resetprop "ro.build.product" "$1"
     resetprop "ro.product.system_ext.device" "$1"
 }
 
 
 setdevicemodel() {
-    resetprop "ro.product.system.device" "$1"
+    resetprop "ro.product.device" "$1"
 
     resetprop "ro.product.model" "$1"
     resetprop "ro.product.odm.model" "$1"
